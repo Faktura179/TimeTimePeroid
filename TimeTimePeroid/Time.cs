659eb59e0f3f2fee2acb7e64adb36f4f38ea46a0
Faktura179 <rfatula12@gmail.com>
2022-05-07 01:11:16 +0200

    Dodaj pliki projektów.

---
+++ TimeTimePeroid/Time.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTimePeroid
+{
+    public struct Time : IEquatable<Time>, IComparable<Time>
+    {
+        public byte Hours { get; init; }
+        public byte Minutes { get; init; }
+        public byte Seconds { get; init; }
+
+        public Time(int hours, int minutes, int seconds)
+        {
+            ValidateTime(hours, minutes, seconds);
+            Hours = (byte)hours;
+            Minutes = (byte)minutes;
+            Seconds = (byte)seconds;
+        }
+
+        public Time(int hours, int minutes)
+        {
+            Seconds = 0;
+            ValidateTime(hours, minutes, Seconds);
+            Hours = (byte)hours;
+            Minutes = (byte)minutes;
+        }
+
+        public Time(int hours)
+        {
+            Minutes = 0;
+            Seconds = 0;
+            ValidateTime(hours, Minutes, Seconds);
+            Hours = (byte)hours;
+        }
+
+        public Time(string time)
+        {
+            if(time is null || string.IsNullOrEmpty(time))
+                throw new ArgumentNullException("Invalid time string");
+            
+            string[] timeParts = time.Split(':');
+
+            int hours = 0;
+            int minutes = 0;
+            int seconds = 0;
+
+            try
+            {
+                hours = Convert.ToInt32(timeParts[0]);
+                if (timeParts.Length > 1)
+                    minutes = Convert.ToInt32(timeParts[1]);
+                if (timeParts.Length > 2)
+                    seconds = Convert.ToInt32(timeParts[2]);
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException("Invalid time string");
+            }
+
+            ValidateTime(hours, minutes, seconds);
+            Hours = (byte)hours;
+            Minutes = (byte)minutes;
+            Seconds = (byte)seconds;
+        }
+
+
+        private static void ValidateTime(int hours, int minutes, int seconds)
+        {
+            if (hours >= 24 || minutes >= 60 || seconds >= 60)
+                throw new ArgumentException("Invalid value");
+            if(hours < 0 || minutes < 0 || seconds < 0)
+                throw new ArgumentException("Invalid value");
+        }
+
+        public override string ToString()
+        {
+            return $"{Hours.ToString().PadLeft(2, '0')}:{Minutes.ToString().PadLeft(2, '0')}:{Seconds.ToString().PadLeft(2, '0')}";
+        }
+
+        public bool Equals(Time other)
+        {
+            if(Hours == other.Hours && Minutes == other.Minutes && Seconds == other.Seconds)
+                return true;
+            return false;
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if (obj == null)
+                return false;
+
+            Time time = (Time)obj;
+            return Equals(time);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Seconds + (byte.MaxValue*Minutes) + (byte.MaxValue*byte.MaxValue* Hours));
+        }
+
+        public static bool operator ==(Time time1, Time time2)
+        {
+            return time1.Equals(time2);
+        }
+
+        public static bool operator !=(Time time1, Time time2)
+        {
+            return !(time1.Equals(time2));
+        }
+
+        public int CompareTo(Time other)
+        {
+            return GetHashCode().CompareTo(other.GetHashCode());
+        }
+
+        public static bool operator <(Time time1, Time time2)
+        {
+            return time1.CompareTo(time2) < 0;
+        }
+        public static bool operator >(Time time1, Time time2)
+        {
+            return time1.CompareTo(time2) > 0;
+        }
+
+        public static bool operator <=(Time time1, Time time2)
+        {
+            return time1.CompareTo(time2) <= 0;
+        }
+
+        public static bool operator >=(Time time1, Time time2)
+        {
+            return time1.CompareTo(time2) >= 0;
+        }
+
+        public static Time operator +(Time time1, Time time2)
+        {
+            int newSeconds = (time1.Seconds + time2.Seconds) % 60;
+            int carryMinutes = (time1.Seconds + time2.Seconds) / 60;
+            int newMinutes = (time1.Minutes + time2.Minutes + carryMinutes) % 60;
+            int carryHours = (time1.Minutes + time2.Minutes + carryMinutes) / 60;
+            int newHours = (time1.Hours + time2.Hours + carryHours) % 24;
+
+            return new Time(newHours, newMinutes, newSeconds);
+        }
+
+        public static Time operator -(Time time1, Time time2)
+        {
+            int newSeconds = (time1.Seconds - time2.Seconds) % 60;
+            int carryMinutes = (time1.Seconds - time2.Seconds - 60) / 60;
+            int newMinutes = (time1.Minutes - time2.Minutes + carryMinutes) % 60;
+            int carryHours = (time1.Minutes - time2.Minutes + carryMinutes - 60) / 60;
+            int newHours = (time1.Hours - time2.Hours + carryHours) % 24;
+
+            return new Time(newHours, newMinutes, newSeconds);
+        }
+    }
+}
---
+++ TimeTimePeroidTests/TimeTests.cs
@@ -0,0 +1,67 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TimeTimePeroid;
+
+namespace TimeTimePeroidTests
+{
+    [TestClass]
+    public class TimeTests
+    {
+        [TestMethod]
+        public void TimeCreate()
+        {
+            Assert.ThrowsException<ArgumentException>(() => new Time(-1, 0, 0));
+            Assert.ThrowsException<ArgumentException>(() => new Time(0, -1, 0));
+            Assert.ThrowsException<ArgumentException>(() => new Time(0, 0, -1));
+
+            Assert.ThrowsException<ArgumentException>(() => new Time(25, 0, 0));
+            Assert.ThrowsException<ArgumentException>(() => new Time(0, 61, 0));
+            Assert.ThrowsException<ArgumentException>(() => new Time(0, 0, 61));
+
+            Assert.ThrowsException<ArgumentException>(() => new Time("-1:00:00"));
+            Assert.ThrowsException<ArgumentException>(() => new Time("00:-1:00"));
+            Assert.ThrowsException<ArgumentException>(() => new Time("00:00:-1"));
+
+            Assert.ThrowsException<ArgumentException>(() => new Time("25:00:00"));
+            Assert.ThrowsException<ArgumentException>(() => new Time("00:61:00"));
+            Assert.ThrowsException<ArgumentException>(() => new Time("00:00:61"));
+
+            Assert.ThrowsException<ArgumentException>(() => new Time("a"));
+            Assert.ThrowsException<ArgumentException>(() => new Time("1:a"));
+            Assert.ThrowsException<ArgumentException>(() => new Time("1:1:a"));
+
+            Assert.IsInstanceOfType(new Time(0, 0, 0), typeof(Time));
+            Assert.IsInstanceOfType(new Time(0, 0), typeof(Time));
+            Assert.IsInstanceOfType(new Time(0), typeof(Time));
+            Assert.IsInstanceOfType(new Time("00:00:00"), typeof(Time));
+        }
+
+        [TestMethod]
+        public void TimeToString()
+        {
+            Assert.AreEqual(new Time(0, 0, 0).ToString(), "00:00:00");
+            Assert.AreEqual(new Time(5, 12, 24).ToString(), "05:12:24");
+            Assert.AreEqual(new Time(12, 6, 3).ToString(), "12:06:03");
+        }
+
+        [TestMethod]
+        public void TimeCompare()
+        {
+            Assert.IsTrue(new Time(0, 0, 0) < new Time(0, 0, 1));
+            Assert.IsTrue(new Time(0, 0, 1) > new Time(0, 0, 0));
+            Assert.IsTrue(new Time(0, 0, 0) <= new Time(0, 0, 1));
+            Assert.IsTrue(new Time(0, 0, 1) >= new Time(0, 0, 0));
+            Assert.IsTrue(new Time(0, 0, 0) >= new Time(0, 0, 0));
+            Assert.IsTrue(new Time(0, 0, 0) <= new Time(0, 0, 0));
+            Assert.IsTrue(new Time(0, 0, 0) == new Time(0, 0, 0));
+            Assert.IsTrue(new Time(0, 1, 0) != new Time(0, 0, 0));
+        }
+
+        [TestMethod]
+        public void TimeCalculations()
+        {
+            Assert.AreEqual(new Time(0, 0, 0) + new Time(0,0,1), new Time(0, 0, 1));
+            Assert.AreEqual(new Time(0, 0, 59) + new Time(0,0,2), new Time(0, 1, 1));
+        }
+    }
+}
